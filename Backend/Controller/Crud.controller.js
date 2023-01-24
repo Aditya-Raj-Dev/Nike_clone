d805a5e17878f45d2365829014d05502a19d3708
Aditya Raj <aadiyadav56837492@gmail.com>
2023-01-24 15:17:05 +0530

    k

--- Backend/Controller/Crud.controller.js
+++ Backend/Controller/Crud.controller.js
@@ -1,46 +1,57 @@
-
-  const Postone=(model)=> async (req,res)=>{
-    try{
-        const data=await model.create(req.body)
-         res.status(201).send({"msg":"successfull","data":data})
-    }
-    catch(e){
-        res.status(500).send({"msg":"failed"})
-    }
+const Postone = (model) => async (req, res) => {
+  try {
+    const data = await model.create(req.body);
+    res.status(201).send({ msg: "successfull", data: data });
+  } catch (e) {
+    res.status(500).send({ msg: "failed" });
   }
-    
-    const PostMany=(model)=> async (req,res)=>{
-        try{
-            const data= await model.insertMany(req.body)
-             res.status(201).send({"msg":"successfull","data":data})
-        }
-        catch(e){
-            res.status(500).send({"msg":"failed"})
-        }
-    }
+};
 
-    const GetAll=(model)=>async (req,res)=>{
-        try{
-            const data=await model.find().lean().exec();
-            res.status(201).send({"msg":"successfull","data":data})
-        }
-        catch(e){
-            res.status(500).send({"msg":"failed"})
-        }
-    }
+const PostMany = (model) => async (req, res) => {
+  try {
+    const data = await model.insertMany(req.body);
+    res.status(201).send({ msg: "successfull", data: data });
+  } catch (e) {
+    res.status(500).send({ msg: "failed" });
+  }
+};
 
-    const DeleteItem=(model)=> async (req,res)=>{
-        try {
-            const data = await model.findByIdAndDelete(req.params.id);
-    
-            return res.status(201).send(data);
-    
-        } catch (e) {
-            res.status(500).send({"msg":"failed"})
-        }
-    }
+const GetAll = (model) => async (req, res) => {
+  try {
+    const data = await model.find().lean().exec();
+    res.status(201).send({ msg: "successfull", data: data });
+  } catch (e) {
+    res.status(500).send({ msg: "failed" });
+  }
+};
 
+const DeleteItem = (model) => async (req, res) => {
+  try {
+    const data = await model.findByIdAndDelete(req.params.id);
 
-    const  PostFavourite=(model)
+    res.status(201).send(data);
+  } catch (e) {
+    res.status(500).send({ msg: "failed" });
+  }
+};
 
-    module.exports={PostMany,Postone,GetAll,DeleteItem }
+const PostFavourite = (model) => async (req, res) => {
+  try {
+    const data = await model.create({ ...req.body, user: req.user._id });
+
+    res.status(201).send(data);
+  } catch (e) {
+    res.status(500).send({ msg: "failed" });
+  }
+};
+
+const getFavourites = (model) => async (req, res) => {
+  try {
+    const data = await model.find({ user: req.user._id });
+    res.status(201).send(data);
+  } catch (e) {
+    res.status(500).send({ msg: "failed" });
+  }
+};
+
+module.exports = { PostMany, Postone, GetAll, DeleteItem,getFavourites,PostFavourite };
